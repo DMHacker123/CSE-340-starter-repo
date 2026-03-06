0b2d791d419acdd64c5a4638916d2674d290a7a2
DMHacker123 <matosalvarezdm@hotmail.com>
2026-03-06 18:57:41 -0400

    Fix port configuration for Render

--- routes/static.js
+++ routes/static.js
@@ -2,23 +2,69 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Home" })
-})
 
-router.get("/custom", (req, res) => {
-  res.render("custom", { title: "Custom" })
+  const vehicles = [
+    { name: "Toyota Camry", image: "camry.jpg" },
+    { name: "Honda Accord", image: "accord.jpg" }
+  ]
+
+  res.render("index", {
+    title: "Home",
+    vehicles
+  })
 })
 
 router.get("/sedan", (req, res) => {
-  res.render("sedan", { title: "Sedan" })
+
+  const vehicles = [
+    { name: "Toyota Camry", image: "/images/vehicles/camaro.jpg" },
+    { name: "Honda Accord", image: "/images/vehicles/delorean.jpg" },
+    { name: "Hyundai Elantra", image: "/images/vehicles/wrangler.jpg" }
+  ]
+
+  res.render("sedan", {
+    title: "Sedan",
+    vehicles
+  })
 })
 
 router.get("/suv", (req, res) => {
-  res.render("suv", { title: "SUV" })
+
+  const vehicles = [
+    { name: "Hummer", image: "/images/vehicles/hummer.jpg" },
+    { name: "Cadillac Escalade", image: "/images/vehicles/escalade.jpg" }
+  ]
+
+  res.render("suv", {
+    title: "SUV",
+    vehicles
+  })
 })
 
 router.get("/truck", (req, res) => {
-  res.render("truck", { title: "Truck" })
+
+  const vehicles = [
+    { name: "Monster Truck", image: "/images/vehicles/monster-truck.jpg" },
+    { name: "Fire Truck", image: "/images/vehicles/fire-truck.jpg" },
+    { name: "Hummer", image: "/images/vehicles/hummer.jpg" }
+  ]
+
+  res.render("truck", {
+    title: "Truck",
+    vehicles
+  })
+})
+
+router.get("/custom", (req, res) => {
+
+  const vehicles = [
+    { name: "Custom Jeep Wrangler", image: "/images/vehicles/wrangler.jpg" }
+  ]
+
+  res.render("custom", {
+    title: "Custom",
+    vehicles
+  })
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,41 +2,37 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
+require("dotenv").config()
+
 const app = express()
-const static = require("./routes/static")
+const staticRoutes = require("./routes/static")
 
 /* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.use(express.static("public")) 
-app.set("layout", "./layouts/layout") // not at views root
+app.use(express.static("public"))
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
-
-// Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.use("/", staticRoutes)
 
 /* ***********************
  * Local Server Information
- * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5500
 
 /* ***********************
- * Log statement to confirm server operation
+ * Server Listener
  *************************/
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
