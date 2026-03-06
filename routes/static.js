157e0a491488dfd6725d3ab4c5f8d22c1695dbe6
DMHacker123 <matosalvarezdm@hotmail.com>
2026-03-05 23:04:20 -0400

    Fix port configuration for Render

--- routes/static.js
+++ routes/static.js
@@ -5,4 +5,20 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Home" })
 })
 
+router.get("/custom", (req, res) => {
+  res.render("custom", { title: "Custom" })
+})
+
+router.get("/sedan", (req, res) => {
+  res.render("sedan", { title: "Sedan" })
+})
+
+router.get("/suv", (req, res) => {
+  res.render("suv", { title: "SUV" })
+})
+
+router.get("/truck", (req, res) => {
+  res.render("truck", { title: "Truck" })
+})
+
 module.exports = router
