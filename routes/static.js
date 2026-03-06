048640e719f26f1915c0d9fadb90da05cd689360
DMHacker123 <matosalvarezdm@hotmail.com>
2026-03-05 22:01:11 -0400

    Fix port configuration for Render

--- routes/static.js
+++ routes/static.js
@@ -1,13 +1,8 @@
-const express = require('express');
-const router = express.Router();
-
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "/public/css"));
-router.use("/js", express.static(__dirname + "/public/js"));
-router.use("/images", express.static(__dirname + "/public/images"));
+const express = require("express")
+const router = express.Router()
 
 router.get("/", (req, res) => {
-  res.render("index"); // or res.send("Home Page Working")
-});
+  res.render("index", { title: "Home" })
+})
 
-module.exports = router;
+module.exports = router
